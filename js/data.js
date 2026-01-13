a023ce5d27403923ca073c4d6231e4112e0aa2ca
AlenaBeloglazova <ms.funduk@mail.ru>
2026-01-13 23:33:54 +1000

    перененосит переменные const в отдельный файл const,js

--- js/data.js
+++ js/data.js
@@ -1,27 +1,15 @@
-import {getUniqueId} from './util.js';
-import {getRandomInteger} from './util.js';
-
-const MESSAGE = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-
-const NAMES = ['Сергей', 'Иван', 'Дмитрий', 'Роман', 'Александр', 'Виктория', 'Ольга', 'Светлана', 'Михаил', 'Анастасия', 'Валентина', 'Петр', 'Виктор',];
-const MIN_LIKES = 15;
-const MAX_LIKES = 200;
-const POSTS_COUNT = 25;
-const MIN_COMMENTS = 0;
-const MAX_COMMENTS = 30;
-
+import {getUniqueId, getRandomInteger} from './util.js';
+import {MESSAGES, NAMES, MIN_LIKES, MAX_LIKES, POSTS_COUNT, MIN_COMMENTS, MAX_COMMENTS} from './const.js';
 
 const createComments = (createPostCommentIdGenerator) => {
-  const randomMessageIndex = getRandomInteger(0, MESSAGE.length - 1);
+  const randomMessageIndex = getRandomInteger(0, MESSAGES.length - 1);
   const randomNameIndex = getRandomInteger(0, NAMES.length - 1);
   const avatarId = getRandomInteger(1, 6);
 
   return {
     id: createPostCommentIdGenerator(),
     avatar: `img/avatar-${ avatarId }.svg`,
-    message: MESSAGE[randomMessageIndex],
+    message: MESSAGES[randomMessageIndex],
     name: NAMES[randomNameIndex],
   };
 };
